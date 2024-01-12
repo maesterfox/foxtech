222c8186a0781b29debceb94fa6c738ba7abe07d
maesterfox <talentedmrfox@hotmail.com>
2024-01-12 06:34:07 +0000

    navbar responsive

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,21 +5,29 @@ import LogoutButton from "./LogoutButton";
 
 export default function Navbar({ user }) {
   return (
-    <nav>
-      <Image
-        src={Logo}
-        alt="Fox Logo"
-        width={90}
-        placeholder="blur"
-        quality={100}
-      />
-      <h1>FoxTech Helpdesk</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/tickets" className="mr-auto">
-        Tickets
-      </Link>
-      {user && <span>Hello, {user.email}</span>}
+    <nav className="flex flex-wrap justify-between items-center p-4">
+      <div className="flex items-center">
+        <Image
+          src={Logo}
+          alt="Fox Logo"
+          s
+          width={90}
+          placeholder="blur"
+          quality={100}
+        />
+        <h1 className="ml-4">FoxTech Helpdesk</h1>
+      </div>
+      {user && <span className="mr-6">Hello, {user.email}</span>}
       <LogoutButton />
+
+      <div className="flex flex-wrap items-center">
+        <Link href="/" className="mr-6">
+          Dashboard
+        </Link>
+        <Link href="/tickets" className="mr-6">
+          Tickets
+        </Link>
+      </div>
     </nav>
   );
 }
