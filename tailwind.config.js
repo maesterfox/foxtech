60cfccdf9f06d8fef12248c1162178f5e5c0a555
maesterfox <talentedmrfox@hotmail.com>
2024-02-22 08:41:39 +0000

    new styles, tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#7856ff",
+        primary: "white",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
